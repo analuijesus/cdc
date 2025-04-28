9a3e79220a79f999a9b7ba53c3162fce0c28490f
Ana de Jesus <anadejesus.dev@gmail.com>
2025-04-28 11:31:53 -0300

    fix: class name

--- src/main/java/br/com/cdc/category/NameCategoryValidator.java
+++ src/main/java/br/com/cdc/category/NameCategoryValidator.java
@@ -5,11 +5,11 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 @Component
-public class NomeCategoryValidator implements Validator {
+public class NameCategoryValidator implements Validator {
 
     private final CategoryRepository repository;
 
-    public NomeCategoryValidator(CategoryRepository repository) {
+    public NameCategoryValidator(CategoryRepository repository) {
         this.repository = repository;
     }
 
